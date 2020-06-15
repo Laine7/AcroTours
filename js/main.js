d9bf1ae1d5906e1f036dd7db1a5aa23263e4bccf
Ellaine <iellainegrace@gmail.com>
2020-06-15 22:53:47 +0930

    updated check fonts please

--- js/main.js
+++ js/main.js
@@ -7,5 +7,22 @@ $(document) .ready (function () {
     $toggleCollapse.click (function () {
         $nav.toggleClass ('uncollapse');
     })
-    $(".owl-carousel") .owlCarousel();
+    $(".owl-carousel") .owlCarousel({
+        mouseDrag:true,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout: 3000,
+        responsiveClass: true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            700:{
+                items:2,
+            },
+            1000: {
+                items:3,
+            }
+        }
+    });
 })
